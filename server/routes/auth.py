5ff08447c5ffafff6ae063ac689b583b6a77ecb4
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-07-06 13:10:00 +0530

    Adding login api

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 import uuid
 import bcrypt
 from models.user import User
+from pydantic_schemas.user_login import UserLogin
 from pydantic_schemas.user_schema import UserCreate
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
@@ -10,7 +11,7 @@ from database import get_db
 
 router = APIRouter()
 
-@router.post("/signup")
+@router.post("/signup",status_code=201)
 def signupuser(user:UserCreate,db:Session=Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
     if user_db:
@@ -20,4 +21,15 @@ def signupuser(user:UserCreate,db:Session=Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+
+@router.post("/login")
+def login_user(user:UserLogin,db:Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(400,"User with the given email does not exist")
+    is_match = bcrypt.checkpw(user.password.encode(),user_db.password)
+
+    if not is_match:
+        raise HTTPException(400,"Incorrect password")
     return user_db
